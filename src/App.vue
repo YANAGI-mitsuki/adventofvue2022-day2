c9a7fabe9213ccc3ee9f99bf888647da414820b3
Mitsuki Yanagi <iganaymjs2@gmail.com>
2023-12-22 01:23:13 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from "vue";
+const jokes = ref([
+  {
+    id: 1,
+    content: '布団が',
+    selected: false
+  },
+  {
+    id: 2,
+    content: '吹っ飛んだ',
+    selected: false
+  }
+])
+</script>
+
+<template>
+<div id="app">
+  <button @click="jokes[0].selected=!jokes[0].selected">tell me!</button><br/>
+  <p v-if="jokes[0].selected">{{ jokes[0].content }}</p>
+  <button @click="jokes[1].selected=!jokes[1].selected">another</button><br/>
+  <p v-if="jokes[1].selected">{{ jokes[1].content }}</p>
+</div>
+</template>
+
+<style scoped>
+
+</style>
