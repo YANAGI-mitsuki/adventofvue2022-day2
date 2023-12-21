fb1ac19785aea83eae4bfc5fce85c0d77222ada7
Mitsuki Yanagi <iganaymjs2@gmail.com>
2023-12-22 02:04:44 +0900

    fix:「tell me!」ボタンを押すと「吹っ飛んだ」が表示されるようにする。

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup>
 import { ref } from "vue";
+const buttonText = ref('tell me!');
+const changeButtonText = () => {
+  buttonText.value = 'another';
+};
 const jokes = ref([
   {
     id: 1,
     content: '布団が',
-    selected: false
+    selected: true
   },
   {
     id: 2,
@@ -12,14 +16,27 @@ const jokes = ref([
     selected: false
   }
 ])
+// const changeText = () => {
+//   jokes[0].content = '吹っ飛んだ'
+// };
+const text = ref()
+const changeText = () => {
+  text.value = '吹っ飛んだ'
+};
+
 </script>
 
 <template>
 <div id="app">
-  <button @click="jokes[0].selected=!jokes[0].selected">tell me!</button><br/>
+  <!-- <button @click="changeButtonText">{{ buttonText }}</button> -->
+  <!-- <button @click="changeText">tell me!</button>
+  <p v-if="jokes[0].selected">{{ jokes[0].content }}</p> -->
+  <button @click="changeText">tell me!</button>
   <p v-if="jokes[0].selected">{{ jokes[0].content }}</p>
-  <button @click="jokes[1].selected=!jokes[1].selected">another</button><br/>
-  <p v-if="jokes[1].selected">{{ jokes[1].content }}</p>
+  <p v-if="jokes[0].selected">{{ text }}</p>
+  <!-- <button @click="jokes[0].selected=!jokes[0].selected">tell me!</button><br/> -->
+  <!-- <button @click="jokes[1].selected=!jokes[1].selected">another</button><br/> -->
+  <!-- <p v-if="jokes[1].selected">{{ jokes[1].content }}</p> -->
 </div>
 </template>
 
